e3e6090752789b059b272d73fb4fe8d2e1d9b17e
tzussman <32444106+tzussman@users.noreply.github.com>
2018-07-07 16:15:48 -0400

    Update JoystickConstants.java
    Continuing cleanup

--- src/constants/JoystickConstants.java
+++ src/constants/JoystickConstants.java
@@ -1,13 +1,5 @@
 package constants;
 
 public class JoystickConstants {
-	public static final int 
-		ACQUIRED_BOX = 7,
-		PREPARE_TO_SCORE_ON_SWITCH = 8,
-		PREPARE_TO_SCORE_ON_SCALE = 9,
-		SCORE = 10,
-		HUNTING = 11,
-		ENABLE_ELEVATOR = 6,
-		MANUAL_ELEVATOR_CONTROL= 5,
-		DEFENSE = 2;
+	
 }
