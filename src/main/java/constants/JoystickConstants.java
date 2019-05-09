397e80ac4525de3373e0276124f32944a68858b1
tzussman <32444106+tzussman@users.noreply.github.com>
2019-05-09 15:31:36 -0400

    Post 2019 update

--- src/main/java/constants/JoystickConstants.java
+++ src/main/java/constants/JoystickConstants.java
@@ -2,4 +2,14 @@ package constants;
 
 public class JoystickConstants {
 	
+	public static final int
+		BUTTONS = 25,
+		NUM_PROFILES = 9;
+	
+	public static class FinalRobotButtons {
+		
+		public static final int
+			DEFAULT = 0;
+	}
+	
 }
