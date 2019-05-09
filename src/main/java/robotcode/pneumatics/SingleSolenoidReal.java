7a29dedafcab7b00c83252de441b08a8b8080f8b
tzussman <32444106+tzussman@users.noreply.github.com>
2019-05-09 11:06:17 -0400

    Post 2019 update
    Only issue w this is that we can't extend from the Solenoid class if we want to use Double Solenoid Value
    Also need to consider Value.kOff

---
+++ src/main/java/robotcode/pneumatics/SingleSolenoidReal.java
@@ -0,0 +1,41 @@
+package robotcode.pneumatics;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class SingleSolenoidReal implements SolenoidInterface {
+	private Solenoid mSingleSolenoid;
+	private Value mCurrent;
+
+	public SingleSolenoidReal(int pPort) {
+		mSingleSolenoid = new Solenoid(pPort);
+		mCurrent = this.getActual();
+	}
+
+	public SingleSolenoidReal(int pPort, int pModuleNumber){
+		mSingleSolenoid = new Solenoid(pModuleNumber, pPort);
+		mCurrent = this.getActual();
+	}
+
+	public void set(Value pDirection) {
+		if(pDirection != mCurrent){
+			// forward maps to true, backward maps to false
+			mSingleSolenoid.set(pDirection == Value.kForward);
+			mCurrent = pDirection;
+		}
+	}
+
+	public Value get() {
+		return mCurrent;
+	}
+
+	public Value getActual() {
+		return mSingleSolenoid.get() ? Value.kForward : Value.kReverse;
+
+	}
+
+	public void setOpposite() {
+		this.set((this.get() == Value.kForward) ? Value.kReverse : Value.kForward);
+	}
+
+}
