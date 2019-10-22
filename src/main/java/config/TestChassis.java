117e69e3c3ce74b3f0acabd37dbf30477ced56e7
Forrest Bicker <bickerforrest@gmail.com>
2019-10-22 15:52:20 -0400

    added AutoConstants to config (why not!

--- src/main/java/config/TestChassis.java
+++ src/main/java/config/TestChassis.java
@@ -29,5 +29,18 @@ public class TestChassis {
 			NUDGE_TURN_SPEED = 0.2;
     }
 
+    // Constants from AutoConstants.DefaultRoutine
+    public static class AutoConstants {	
+		public final static double 
+		WHEEL_ANGLE = 0,
+		MINIMUM_SPEED = 0.3,
+		MAXIMUM_SPEED = 0.3;
+		
+		public final static long 
+		ACCELERATION_TIME = 1000,
+		DRIVE_FULL_SPEED_TIME = 1000,
+		DECELERATION_TIME = 1000;	
+	}
+
 	public static Object ports;
 }
