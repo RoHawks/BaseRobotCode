24a260ae4b7d228293127ff2cf2d5be8a03fd535
jrmkim50 <jrmkim50@gmail.com>
2019-11-06 17:28:13 -0500

    added spacing to sparkmax

--- src/main/java/robotcode/systems/SparkMaxMotor.java
+++ src/main/java/robotcode/systems/SparkMaxMotor.java
@@ -16,31 +16,40 @@ public class SparkMaxMotor implements IMotorWithEncoder {
     public void setOutput(double percentage) {
         spark.set(percentage);
     }
+
     public void setAdd180(boolean add180) {
         
     }
+
     public void setVelocity(double velocity) {
         double speed = Math.signum(velocity) * Math.abs(velocity);
 		spark.set(speed);
     }
+
     public void setPosition(int position) {
         spark.getEncoder().setPosition(position);
     }
+
     public int getPosition() {
         return spark.getEncoder().getPosition();
     }
+
     public void setInverted(boolean inverted) {
         spark.setInverted(inverted);
     }
+
     public boolean getInverted() {
         return spark.getInverted();
     }
+
     public int getSelectedSensorPosition(int port) {
         return 0;
     }
+
     public double getVelocity() {
         return spark.getEncoder().getPosition();
     }
+    
     public boolean getAdd180() {
         return false;
     }
