ecfc065e4b9dd45a55c14bc5a6053956aae2d6c3
Forrest Bicker <bickerforrest@gmail.com>
2019-11-11 20:26:54 -0500

    added spark inversion

--- src/main/java/drivetrain/controllers/SparkMax.java
+++ src/main/java/drivetrain/controllers/SparkMax.java
@@ -8,7 +8,8 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class SparkMax implements IMotorWithEncoder {
 
     private CANSparkMax spark;
-
+    protected boolean isInverted;
+    
     public SparkMax(int port) {
         spark = new CANSparkMax(port, MotorType.kBrushless);
     }
@@ -31,4 +32,13 @@ public class SparkMax implements IMotorWithEncoder {
     public int getPosition() {
         return (int)spark.getEncoder().getPosition();
     }
+
+    public boolean getInverted() {
+        return spark.getInverted();
+    }
+
+    public void setInverted(boolean inverted) {
+        spark.setInverted(inverted);
+    }
+
 }
