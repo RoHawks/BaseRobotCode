47a365cece0159ea5551ec23412d88e47c82817a
Forrest Bicker <bickerforrest@gmail.com>
2019-11-13 18:08:57 -0500

    added getOutput to IMotor

--- src/main/java/drivetrain/interfaces/IMotor.java
+++ src/main/java/drivetrain/interfaces/IMotor.java
@@ -2,6 +2,7 @@ package drivetrain.interfaces;
 
 public interface IMotor {
     void setOutput(double value);
+    double getOutput();
 
     void setInverted(boolean inverted);
     boolean getInverted();
