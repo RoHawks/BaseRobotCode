21357b175d634b0f44fa34412ba94fc8fb01250a
morvayeszter <esztermorvay34@gmail.com>
2019-11-20 17:57:04 -0500

    added a comment

--- src/main/java/drivetrain/controllers/configs/TalonSRXConfig.java
+++ src/main/java/drivetrain/controllers/configs/TalonSRXConfig.java
@@ -2,5 +2,5 @@ package drivetrain.controllers.configs;
 
 public class TalonSRXConfig {
     public int port;
-    public boolean inverted;
+    public boolean inverted; //clockwise or counterclockwise
 }
