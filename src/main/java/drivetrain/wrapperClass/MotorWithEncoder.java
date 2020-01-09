8a5ca59ac84d9952d45c932c087c159ec7513b57
Forrest Bicker <bickerforrest@gmail.com>
2020-01-08 21:43:14 -0500

    removed arbitrary throws and nonexistent references

--- src/main/java/drivetrain/wrapperClass/MotorWithEncoder.java
+++ src/main/java/drivetrain/wrapperClass/MotorWithEncoder.java
@@ -8,15 +8,12 @@ import resource.ResourceFunctions;
 public class MotorWithEncoder implements IMotorWithEncoder {
     protected IMotorWithEncoder motorWithEncoder;
 
-    protected int sensorPosition = 0;
-    protected boolean isReversed = false;
+    // protected int sensorPosition = 0;
     protected double TICKS_PER_ROTATION = 0;
-    protected double offset = 0;
 
-    public MotorWithEncoder(IMotorWithEncoder motorWithEncoder, double ticksPerRotation, double offset) {
+    public MotorWithEncoder(IMotorWithEncoder motorWithEncoder, double ticksPerRotation) {
         this.motorWithEncoder = motorWithEncoder;
         this.TICKS_PER_ROTATION = ticksPerRotation;
-        this.offset = offset;
     }
 
     @Override
@@ -44,32 +41,32 @@ public class MotorWithEncoder implements IMotorWithEncoder {
     }
 
     @Override
-    public void setRawPosition(double ticks) throws IOException {
+    public void setRawPosition(double ticks) {
         motorWithEncoder.setRawPosition(ticks);
     }
 
     @Override
-    public void setOffsetPosition(double ticks) throws IOException {
+    public void setOffsetPosition(double ticks) {
         motorWithEncoder.setOffsetPosition(ticks);
     }
 
     @Override
-    public double getRawPosition() throws IOException {
+    public double getRawPosition() {
         return motorWithEncoder.getRawPosition();
     }
 
     @Override
-    public double getOffsetPosition() throws IOException {
+    public double getOffsetPosition() {
         return motorWithEncoder.getOffsetPosition();
     }
 
     @Override
-    public void setVelocity(double velocity) throws IOException {
+    public void setVelocity(double velocity) {
         motorWithEncoder.setVelocity(velocity);
     }
 
     @Override
-    public double getVelocity() throws IOException {
+    public double getVelocity() {
         return motorWithEncoder.getVelocity();
     }
 
@@ -78,13 +75,9 @@ public class MotorWithEncoder implements IMotorWithEncoder {
         motorWithEncoder.setReversed(reversed);
     }
 
-    /**
-    * returns wether or not the motor is reversed
-    * @return the reversal of the motor
-    */
     @Override
     public boolean getReversed() {
-        return isReversed;
+        return motorWithEncoder.getReversed();
     }
 
     /**
@@ -94,20 +87,20 @@ public class MotorWithEncoder implements IMotorWithEncoder {
      * @param angle the desired position of the motor
      */
     @Override
-    public void setReversedOffsetAngle(double angle) throws IOException {
+    public void setReversedOffsetAngle(double angle) {
         double target = ResourceFunctions.putAngleInRange(angle);
         double current = getReversedOffsetAngle();
         double delta = target - current;
         // reverse the motor if turning more than 90 degrees away in either direction
         if (Math.abs(delta) > 90 && Math.abs(delta) < 270) {
             delta += 180;
-            isReversed = !isReversed;
+            setReversed(!getReversed());
         }
         setRawAngle(getRawAngle() + ResourceFunctions.putAngleInRange(delta));
     }
 
     @Override
-    public void setOffsetAngle(double angle) throws IOException {
+    public void setOffsetAngle(double angle) {
         double target = ResourceFunctions.putAngleInRange(angle);
         double delta = target - getOffsetAngle();
         setRawAngle(getRawAngle() + ResourceFunctions.putAngleInRange(delta));
@@ -120,7 +113,7 @@ public class MotorWithEncoder implements IMotorWithEncoder {
     * @param angle the desired position of the motor
     */
     @Override
-    public void setRawAngle(double angle) throws IOException {
+    public void setRawAngle(double angle) {
         double target = ResourceFunctions.putAngleInRange(angle);
         double delta = target - getRawAngle();
 
@@ -134,7 +127,7 @@ public class MotorWithEncoder implements IMotorWithEncoder {
     }
 
     @Override
-    public double getReversedOffsetAngle() throws IOException {
+    public double getReversedOffsetAngle() {
         double angle = getOffsetAngle();
         if (getReversed()) {
             if (angle >= 180) {
@@ -152,7 +145,7 @@ public class MotorWithEncoder implements IMotorWithEncoder {
      * @return the angle of the motor
      */
     @Override
-    public double getOffsetAngle() throws IOException {
+    public double getOffsetAngle() {
         double offsetTicks = getOffsetPosition();
         double offsetAngle = ticksToDegrees(offsetTicks);
         return ResourceFunctions.putAngleInRange(offsetAngle);
@@ -164,7 +157,7 @@ public class MotorWithEncoder implements IMotorWithEncoder {
      * @return the absolute rotation of the motor
      */
     @Override
-    public double getRawAngle() throws IOException {
+    public double getRawAngle() {
         return ResourceFunctions.putAngleInRange(ticksToDegrees(getRawPosition()));
     }
 
