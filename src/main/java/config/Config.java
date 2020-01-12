e43c393f9b932ba3c50da3e67d23eef1e5038391
jrmkim50 <jrmkim50@gmail.com>
2020-01-12 11:56:16 -0500

    Added lift Constants

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -11,6 +11,7 @@ public class Config {
     public SwerveSpeeds swerveSpeeds = new SwerveSpeeds();
     public WheelConfig[] wheelConfigs = new WheelConfig[4];
     public IntakeConstants intakeConstants = new IntakeConstants(); 
+    public LiftConstants liftConstants = new LiftConstants(); 
 
     //Constants for the intake test mechanism
     public class IntakeConstants {    
@@ -24,13 +25,27 @@ public class Config {
             SPEED_INCREMENT = .1;
     }
 
+    public class LiftConstants {    
+        public final int 
+            LIFT_PORT = 5,
+            LIFT_UP_BUTTON = 4, //Y button
+            LIFT_DOWN_BUTTON = 1; //A button
+        public final boolean LIFT_INVERTED = true;
+        public final boolean HAS_TOP_LIMIT_SWITCH = true;
+        public final boolean HAS_BOTTOM_LIMIT_SWITCH = true;
+        public final double 
+            LIFT_POWER_OUTPUT = 0,
+            SPEED_INCREMENT = .1;
+    }
+
     // Constatnts from RunConstants
     public class RunConstants {  
         public boolean RUNNING_DRIVE,
             RUNNING_PNEUMATICS,
             RUNNING_CAMERA,
             SECONDARY_JOYSTICK,
-            RUNNING_INTAKE;
+            RUNNING_INTAKE, 
+            RUNNING_LIFT;
     }
 
     // Constants from DriveConstants
