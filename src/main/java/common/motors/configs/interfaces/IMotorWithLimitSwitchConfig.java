674d091ebbda9d5d67cdfda80f0e7983681967d2
James Kersh <james.kersh@rutgers.edu>
2020-01-13 17:50:51 -0500

    Delete IMotorWithLimitSwitchConfig.java

--- src/main/java/common/motors/configs/interfaces/IMotorWithLimitSwitchConfig.java
+++
@@ -1,8 +0,0 @@
-package common.motors.configs.interfaces;
-
-import common.motors.interfaces.IMotorWithLimitSwitch;
-
-public interface IMotorWithLimitSwitchConfig extends IMotorConfig {
-    IMotorConfig getMotorConfig();
-    IMotorWithLimitSwitch build();
-}
