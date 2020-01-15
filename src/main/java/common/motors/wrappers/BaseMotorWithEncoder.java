eb70910e26d28e4809684b73b0ede7df975b24a3
Forrest Bicker <bickerforrest@gmail.com>
2020-01-15 18:14:08 -0500

    added abstract methods to BaseMotorWithEncoder

--- src/main/java/common/motors/wrappers/BaseMotorWithEncoder.java
+++ src/main/java/common/motors/wrappers/BaseMotorWithEncoder.java
@@ -1,6 +1,7 @@
 package common.motors.wrappers;
 
 import common.motors.interfaces.IMotorWithEncoder;
+import resource.ResourceFunctions;
 
 public abstract class BaseMotorWithEncoder extends BaseMotor implements IMotorWithEncoder {
     boolean[] config;
@@ -11,4 +12,22 @@ public abstract class BaseMotorWithEncoder extends BaseMotor implements IMotorWi
         this.config = config;
     }
 
+    public abstract void setRawPosition(double rawTicks);
+    public abstract void setOffsetPosition(double rawTicks);
+
+    public abstract double getRawPosition();
+
+    public double getOffsetPosition() {
+        return getRawPosition() - getOffset();
+    }
+
+    public abstract void setVelocity(double velocity);
+    public abstract double getVelocity();
+
+    public abstract void setReversed();
+    public abstract boolean getReversed();
+
+    public abstract double getOffset();
+
+    public abstract double getTicksPerRotation();
 }
