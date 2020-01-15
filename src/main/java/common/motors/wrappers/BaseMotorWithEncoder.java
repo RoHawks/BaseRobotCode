b3be2bf195b494efc00da317e3872a0cc6eeff1e
Forrest Bicker <bickerforrest@gmail.com>
2020-01-15 18:15:59 -0500

    implemented concrete BaseMotorWithEncoder methods

--- src/main/java/common/motors/wrappers/BaseMotorWithEncoder.java
+++ src/main/java/common/motors/wrappers/BaseMotorWithEncoder.java
@@ -13,6 +13,7 @@ public abstract class BaseMotorWithEncoder extends BaseMotor implements IMotorWi
     }
 
     public abstract void setRawPosition(double rawTicks);
+
     public abstract void setOffsetPosition(double rawTicks);
 
     public abstract double getRawPosition();
@@ -22,12 +23,109 @@ public abstract class BaseMotorWithEncoder extends BaseMotor implements IMotorWi
     }
 
     public abstract void setVelocity(double velocity);
+
     public abstract double getVelocity();
 
     public abstract void setReversed();
+
     public abstract boolean getReversed();
 
     public abstract double getOffset();
 
     public abstract double getTicksPerRotation();
+
+    protected double ticksToDegrees(double ticks) {
+        return (ticks / getTicksPerRotation()) * 360D;
+    }
+
+    protected double degreesToTicks(double degrees) {
+        return (degrees / 360D) * getTicksPerRotation();
+    }
+
+    /**
+     * sets the angle of the motor
+     * takes into account motor offset
+     * includes optimization for the reversal of the turn
+     * @param angle the desired position of the motor
+     */
+    public void setReversedOffsetAngle(double angle) {
+        double target = ResourceFunctions.putAngleInRange(angle);
+        double current = getReversedOffsetAngle();
+        double delta = target - current;
+        // reverse the motor if turning more than 90 degrees away in either direction
+        if (Math.abs(delta) > 90 && Math.abs(delta) < 270) {
+            delta += 180;
+            setReversed(!getReversed());
+        }
+        setRawAngle(getRawAngle() + ResourceFunctions.putAngleInRange(delta));
+    }
+
+    @Override
+    public void setOffsetAngle(double angle) {
+        double target = ResourceFunctions.putAngleInRange(angle);
+        double delta = target - getOffsetAngle();
+        setRawAngle(getRawAngle() + ResourceFunctions.putAngleInRange(delta));
+    }
+
+    /**
+    * sets the angle of the motor
+    * does not take into account motor offset
+    * includes optimization for direction of the turn
+    * @param angle the desired position of the motor
+    */
+    @Override
+    public void setRawAngle(double angle) {
+        double target = ResourceFunctions.putAngleInRange(angle);
+        double delta = target - getRawAngle();
+
+        if (delta > 180) {
+            delta -= 360;
+        } else if (delta < -180) {
+            delta += 360;
+        }
+
+        setRawPosition(getRawPosition() + degreesToTicks(delta));
+    }
+
+    /**
+    * returns the current angle of the motor
+    * takes into account motor offset
+    * takes into account the reversal of the motor
+    * @param angle the desired position of the motor
+    */
+    @Override
+    public double getReversedOffsetAngle() {
+        double angle = getOffsetAngle();
+        if (getReversed()) {
+            if (angle >= 180) {
+                angle -= 180;
+            } else {
+                angle += 180;
+            }
+        }
+        return angle;
+    }
+
+    /**
+     * returns the current angle of the motor
+     * takes into account motor offset
+     * takes into account the reversal of the motor
+     * @return the angle of the motor
+     */
+    @Override
+    public double getOffsetAngle() {
+        double offsetTicks = getOffsetPosition();
+        double offsetAngle = ticksToDegrees(offsetTicks);
+        return ResourceFunctions.putAngleInRange(offsetAngle);
+    }
+
+    /**
+     * returns the current angle of the motor
+     * does not take into account motor offset
+     * @return the absolute rotation of the motor
+     */
+    @Override
+    public double getRawAngle() {
+        return ResourceFunctions.putAngleInRange(ticksToDegrees(getRawPosition()));
+    }
 }
