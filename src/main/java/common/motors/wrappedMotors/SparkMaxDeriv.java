a6d54e000da1a0e3a7e02f01b8da5cffc770648c
Forrest Bicker <bickerforrest@gmail.com>
2020-01-15 19:56:09 -0500

    removed superfluous imports

--- src/main/java/common/motors/wrappedMotors/SparkMaxDeriv.java
+++ src/main/java/common/motors/wrappedMotors/SparkMaxDeriv.java
@@ -4,19 +4,13 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import common.motors.configs.interfaces.IMotorConfig;
 import common.motors.configs.interfaces.IMotorWithEncoderConfig;
-import common.motors.interfaces.IMotorWithEncoder;
 import common.motors.wrappers.BaseMotorWithEncoder;
-import resource.ResourceFunctions;
 
-// We need to change the ticks from int to double because sparks use revolutions instead of ticks
-
-public class SparkMaxDeriv extends BaseMotorWithEncoder { // implements IMotorWithEncoder
+public class SparkMaxDeriv extends BaseMotorWithEncoder { // ? should this implement IMotorWithEncoder
 
     private CANSparkMax spark;
     protected boolean isReversed;
-    protected double offset; // offset in ticks
 
     protected static final double TICKS_PER_ROTATION = 1;
 
