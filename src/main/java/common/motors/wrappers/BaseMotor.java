f31a7fc3caa339aaeb2b500b531aae6a1116eff2
Forrest Bicker <bickerforrest@gmail.com>
2020-01-15 19:42:36 -0500

    re-abstracted inversion methods on BaseMotor

--- src/main/java/common/motors/wrappers/BaseMotor.java
+++ src/main/java/common/motors/wrappers/BaseMotor.java
@@ -4,19 +4,12 @@ import common.motors.configs.interfaces.IMotorConfig;
 import common.motors.interfaces.IMotor;
 
 public abstract class BaseMotor implements IMotor {
-    boolean isInverted;
-
     public BaseMotor(IMotorConfig config) {
-        isInverted = config.getInverted();
     }
 
-    public void setInverted(boolean inverted) {
-        isInverted = inverted;
-    }
+    public abstract void setInverted(boolean inverted);
 
-    public boolean getInverted() {
-        return isInverted;
-    }
+    public abstract boolean getInverted();
 
     public abstract void setOutput(double value);
 
