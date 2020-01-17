90d2cfe1c5b20ba5b973dc51957aa263f86a6aab
jrmkim50 <jrmkim50@gmail.com>
2020-01-17 00:47:09 -0500

    Re-implemented the updated ILogger by importing ILogLocation. Gave each ILogLocation the ability to store messages.

--- src/main/java/logging/Logger.java
+++ src/main/java/logging/Logger.java
@@ -2,6 +2,7 @@ package logging;
 
 import logging.configs.interfaces.ILoggerConfig;
 import logging.enums.LogLevel;
+import logging.interfaces.ILogLocation;
 import logging.interfaces.ILogMessage;
 import logging.interfaces.ILogger;
 
@@ -13,35 +14,37 @@ public class Logger implements ILogger {
     }
 
     @Override
-    public void log(LogLevel level, ILogMessage message) {
+    public void log(LogLevel level, ILogMessage message, ILogLocation location) {
+        //ILogMessage message = new SmartDashboardMessage(String key, String value);
+        //message.storeMessage();
         if(level.compareTo(config.getMinLogLevel()) >= 0) {
-            //TODO: log it somewhere!!
+            location.storeMessage(message);
         }
     }
 
     @Override
-    public void trace(ILogMessage message) {
-        log(LogLevel.Trace, message);
+    public void trace(ILogMessage message, ILogLocation location) {
+        log(LogLevel.Trace, message, location);
     }
 
     @Override
-    public void debug(ILogMessage message) {
-        log(LogLevel.Debug, message);
+    public void debug(ILogMessage message, ILogLocation location) {
+        log(LogLevel.Debug, message, location);
     }
 
     @Override
-    public void info(ILogMessage message) {
-        log(LogLevel.Info, message);
+    public void info(ILogMessage message, ILogLocation location) {
+        log(LogLevel.Info, message, location);
     }
 
     @Override
-    public void warning(ILogMessage message) {
-        log(LogLevel.Warning, message);
+    public void warning(ILogMessage message, ILogLocation location) {
+        log(LogLevel.Warning, message, location);
     }
 
     @Override
-    public void error(ILogMessage message) {
-        log(LogLevel.Error, message);
+    public void error(ILogMessage message, ILogLocation location) {
+        log(LogLevel.Error, message, location);
 	}
 
 }
