0487cd1e0b655816e388b2f2b60d057497e2ff8e
morvayeszter <esztermorvay34@gmail.com>
2020-01-18 14:30:42 -0500

    added ArrayList destinations to LoggerConfig

--- src/main/java/logging/configs/LoggerConfig.java
+++ src/main/java/logging/configs/LoggerConfig.java
@@ -1,13 +1,20 @@
 package logging.configs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import logging.configs.interfaces.ILoggerConfig;
 import logging.enums.LogLevel;
+import logging.interfaces.ILogDestination;
 
 public class LoggerConfig implements ILoggerConfig {
     protected final LogLevel minLevel;
+    private List<ILogDestination> destinations = new ArrayList<ILogDestination>(); 
+    
 
-    public LoggerConfig(LogLevel minLevel) {
+    public LoggerConfig(List<ILogDestination> destinations, LogLevel minLevel) {
         this.minLevel = minLevel;
+        this.destinations = destinations;
     }
 
     @Override
@@ -15,4 +22,5 @@ public class LoggerConfig implements ILoggerConfig {
         return minLevel;
 	}
 
+ 
 }
