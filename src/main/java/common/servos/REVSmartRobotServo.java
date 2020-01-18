73fb5ab961db130463ae0d3f2d0a74a89e8e046d
Forrest Bicker <bickerforrest@gmail.com>
2020-01-18 12:08:44 -0500

    implemented IServo methods with RevSRS

--- src/main/java/common/servos/REVSmartRobotServo.java
+++ src/main/java/common/servos/REVSmartRobotServo.java
@@ -2,18 +2,55 @@ package common.servos;
 
 import edu.wpi.first.wpilibj.PWM;
 
-public class REVSmartRobotServo extends PWM {
+public class REVSmartRobotServo extends PWM implements IServo {
     private double maxAngle = 0;
     private double minAngle = 180;
 
-    private double maxPulaseFreq = 2.5; // milisecconds
-    private double minPulseFreq = .5;
+    private double maxPeriod = 2.5; // milisecconds
+    private double minPeriod = 0.5;
 
     public REVSmartRobotServo(int channel) {
         super(channel);
-        setBounds(maxPulaseFreq, 0, 0, 0, minPulseFreq);
+        setBounds(maxPeriod, 0, 0, 0, minPeriod);
         setPeriodMultiplier(PeriodMultiplier.k4X);
     }
 
+    @Override
+    public void setRawPosition(double value) {
+        setPosition(value); // position from 0 to 1 out of full range
+    }
 
+    @Override
+    public double getRawPosition() {
+        return getPosition();
+    }
+
+    @Override
+    public void setRawAngle(double angle) {
+        if (angle < minAngle) {
+            angle = minAngle;
+        } else if (angle > maxAngle) {
+            angle = maxAngle;
+        }
+
+        double target = (angle - minAngle) / (maxAngle - minAngle);
+
+        setRawPosition(target);
+    }
+
+    @Override
+    public double getRawAngle() {
+        return getRawPosition() * (maxAngle - minAngle) + minAngle;
+    }
+
+    // TODO: abstract servo things
+    @Override
+    public void setVelocity(double velocity) {
+        setSpeed(velocity);
+    }
+
+    @Override
+    public double getVelocity() {
+        return getSpeed();
+    }
 }
