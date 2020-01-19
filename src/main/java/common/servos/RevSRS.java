65f1712985fcbc2591a3181e38cfa27293e4d78a
Forrest Bicker <bickerforrest@gmail.com>
2020-01-18 20:00:50 -0500

    added HAL to RevSRS

--- src/main/java/common/servos/RevSRS.java
+++ src/main/java/common/servos/RevSRS.java
@@ -1,13 +1,16 @@
 package common.servos;
 
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Servo;
 
 public class RevSRS extends PWM implements IServo {
     private double maxAngle;
     private double minAngle;
 
-    private double maxPeriod = 2.5 ; // milisecconds
-    private double minPeriod = 0.5;
+    private double maxPeriod = 2500 / 1000; // milisecconds
+    private double minPeriod = 500 / 1000;
 
     public RevSRS(int channel, double maxAngle, double minAngle) {
         super(channel);
@@ -15,6 +18,11 @@ public class RevSRS extends PWM implements IServo {
         this.minAngle = minAngle;
         setBounds(maxPeriod, 0, 0, 0, minPeriod);
         setPeriodMultiplier(PeriodMultiplier.k4X);
+
+        // idk if these do anything
+        HAL.report(tResourceType.kResourceType_Servo, getChannel());
+        setName("Servo", getChannel()); 
+
     }
 
     @Override
@@ -47,8 +55,8 @@ public class RevSRS extends PWM implements IServo {
 
     // TODO: abstract servo things
     @Override
-    public void setOutput(double velocity) {
-        setSpeed(velocity);
+    public void setOutput(double output) {
+        setSpeed(output);
     }
 
     @Override
