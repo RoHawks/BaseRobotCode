883a542c32384411545cd62a445cdab0a2e76d80
Forrest Bicker <bickerforrest@gmail.com>
2020-02-08 11:47:19 -0500

    reworking temporary

--- src/main/java/common/motors/BaseMotorWithEncoder.java
+++ src/main/java/common/motors/BaseMotorWithEncoder.java
@@ -10,11 +10,12 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
                                            implements IMotorWithEncoder {
     protected boolean isReversed;
     protected double offset;
+    protected double target;
 
     protected BaseMotorWithEncoder(IMotorWithEncoderConfig<TMotor, TMotorConfig> config) {
         offset = config.getEncoderConfig().getOffset();
 
-        var startAngle = getOffsetAngle();
+        double startAngle = getOffsetAngle();
         isReversed = startAngle > 90 && startAngle < 270 ? true : false;
     }
     
@@ -23,6 +24,7 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
     }
     
     protected abstract double getTicksPerRotation();
+    public abstract double getRawPosition();
 
     /**
      * reverses the virtual front of the motor
@@ -43,66 +45,84 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
         return isReversed;
     }
 
-    /**
-     * sets the angle of the motor
-     * takes into account motor offset
-     * includes optimization for the reversal of the turn
-     * @param angle the desired position of the motor
-     */
-    @Override
-    public void setReversedOffsetAngle(double angle) {
-        double target = ResourceFunctions.putAngleInRange(angle);
-        double current = getReversedOffsetAngle();
-        double delta = target - current;
-        // reverse the motor if turning more than 90 degrees away in either direction
-        if (Math.abs(delta) > 90 && Math.abs(delta) < 270) {
-            //if difference between current and target is > 90
-            isReversed = !isReversed;
-            delta += 180;
-            
-        }
-        setRawAngle(getRawAngle() + ResourceFunctions.putAngleInRange(delta));
+    public void setTarget(double value) {
+        setTarget(value, false, false);
     }
 
-    @Override
-    public void setOffsetAngle(double angle) {
-        double target = ResourceFunctions.putAngleInRange(angle);
-        double delta = target - getOffsetAngle();
-        setRawAngle(getRawAngle() + ResourceFunctions.putAngleInRange(delta));
-    }
-
-    /**
-    * sets the angle of the motor
-    * does not take into account motor offset
-    * includes optimization for direction of the turn
-    * @param angle the desired position of the motor
-    */
-    @Override
-    public void setRawAngle(double angle) {
-        double target = ResourceFunctions.putAngleInRange(angle);
-        double delta = target - getRawAngle();
-
-        if (delta > 180) {
-            delta -= 360;
-        } else if (delta < -180) {
-            delta += 360;
+    public void setTarget(double value, boolean withTicks, boolean withRaw) {
+        if (!withTicks) {
+            value = degreesToTicks(value);
         }
 
-        setRawPosition(getRawPosition() + degreesToTicks(delta));
+        if (!withRaw) {
+            value -= offset;
+        }
+
+        // target is now in raw ticks
+        this.target = ResourceFunctions.putAngleInRange(value); 
     }
 
-    @Override
-    public double getReversedOffsetAngle() {
-        var angle = getOffsetAngle();
-        if (isReversed) {
-            if (angle >= 180) {
-                angle -= 180;
-            } else {
-                angle += 180;
+    public double getTarget() {
+        return getTarget(false, false);
+    }
+
+    public double getTarget(boolean withTicks, boolean withRaw) {
+        double out = target;
+
+        if (!withTicks) {
+            out = ticksToDegrees(out);
+        }
+
+        if (!withRaw) {
+            out += offset;
+        }
+
+        return ResourceFunctions.putAngleInRange(out);
+    }
+
+    public void update(boolean withReversal, boolean withDirectional) {
+        double lTarget = target;
+
+        if (withReversal) {
+            double delta = lTarget - getPosition();
+            if (Math.abs(delta) > 90 && Math.abs(delta) < 270) {
+                //if difference between current and target is > 90
+                isReversed = !isReversed; // TODO: move out of motor?
+                delta += 180;
             }
+            lTarget += delta;
         }
-        return angle;
+
+        if (withDirectional) {
+            double delta = lTarget - getPosition();
+
+            if (delta > 180) {
+                delta -= 360;
+            } else if (delta < -180) {
+                delta += 360;
+            }
+            lTarget += delta;
+        }
+
+        setPosition(lTarget);
     }
+
+    public abstract double getPosition();
+
+    public double getPosition(boolean withTicks, boolean withRaw) {
+        double position = getPosition();
+
+        if (!withTicks) {
+            position = ticksToDegrees(position);
+        }
+
+        if (!withRaw) {
+            position += offset;
+        }
+
+        return position;
+    }
+
 
     /**
      * returns the current angle of the motor
@@ -112,7 +132,7 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
     @Override
     public double getOffsetAngle() {
         double offsetTicks = getOffsetPosition();
-        var offsetAngle = ticksToDegrees(offsetTicks);
+        double offsetAngle = ticksToDegrees(offsetTicks);
         return ResourceFunctions.putAngleInRange(offsetAngle);
     }
 
