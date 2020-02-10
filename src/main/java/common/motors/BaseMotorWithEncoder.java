1cbe564d2eebb263f478336ae81ddd15cef85cce
Forrest Bicker <bickerforrest@gmail.com>
2020-02-09 23:20:21 -0500

    consolidated duplicate motor methods into multipurpose parameter-controlled methods

--- src/main/java/common/motors/BaseMotorWithEncoder.java
+++ src/main/java/common/motors/BaseMotorWithEncoder.java
@@ -5,9 +5,8 @@ import common.motors.configs.interfaces.IMotorWithEncoderConfig;
 import common.motors.interfaces.IMotorWithEncoder;
 import resource.ResourceFunctions;
 
-public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder, 
-                                           TMotorConfig extends IMotorConfig<TMotor>> 
-                                           implements IMotorWithEncoder {
+public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder, TMotorConfig extends IMotorConfig<TMotor>> implements IMotorWithEncoder {
+
     protected boolean isReversed;
     protected double offset;
     protected double targetPosition;
@@ -24,10 +23,9 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
     }
 
     public enum RotationMode {
-        Absolute,
-        Angular;
+        Absolute, // ticks
+        Angular, // degrees
     }
-
 
     protected BaseMotorWithEncoder(IMotorWithEncoderConfig<TMotor, TMotorConfig> config) {
         offset = config.getEncoderConfig().getOffset();
@@ -35,12 +33,13 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
         double startAngle = getOffsetAngle();
         isReversed = startAngle > 90 && startAngle < 270 ? true : false;
     }
-    
+
     protected BaseMotorWithEncoder(IMotorConfig<TMotor> config) {
         //this only exists so that child classes can create non-encoder instances
     }
-    
+
     protected abstract double getTicksPerRotation();
+
     public abstract double getRawPosition();
 
     /**
@@ -62,8 +61,8 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
         return isReversed;
     }
 
-    public void setTargetPosition(double value) {
-        setTargetPosition(value, Unit.Degrees, Type.Offset);
+    public void setTargetPosition(double target) {
+        setTargetPosition(target, Unit.Degrees, Type.Offset);
     }
 
     public void setTargetPosition(double target, Unit unit, Type type) {
@@ -76,9 +75,73 @@ public abstract class BaseMotorWithEncoder<TMotor extends IMotorWithEncoder,
         }
 
         // target is now in raw ticks
-        this.targetPosition = target; 
+        this.targetPosition = target;
     }
 
+    public double getTargetPosition() {
+        return getTargetPosition(Unit.Degrees, Type.Offset);
+    }
+
+    public double getTargetPosition(Unit unit, Type type) {
+        double target = targetPosition;
+
+        if (type == Type.Offset) {
+            target += offset;
+        }
+
+        if (unit == Unit.Degrees) {
+            target = ticksToDegrees(target);
+        }
+
+        return ResourceFunctions.putAngleInRange(target);
+    }
+
+    public void updatePosition(RotationMode mode) {
+        double target = targetPosition;
+
+        switch (mode) {
+        case Absolute:
+            break;
+        case Angular:
+            double delta = target - getNativePosition();
+            if (Math.abs(delta) > 90 && Math.abs(delta) < 270) { // if difference between current and target is > 90
+                isReversed = !isReversed; // TODO: move out of motor-level?
+                delta += 180;
+            }
+
+            target += delta;
+
+            delta = target - getNativePosition();
+
+            if (delta > 180) {
+                delta -= 360;
+            } else if (delta < -180) {
+                delta += 360;
+            }
+            target += delta;
+            target = ResourceFunctions.putAngleInRange(target);
+            break;
+        }
+        setNativePosition(target);
+    }
+
+    public abstract void setNativePosition(double target); // native method in raw ticks
+
+    public abstract double getNativePosition(); // native method in raw ticks
+
+    public double getPosition(Unit unit, Type type) {
+        double position = getNativePosition();
+
+        if (unit == Unit.Degrees) {
+            position = ticksToDegrees(position);
+        }
+
+        if (type == Type.Offset) {
+            position += offset;
+        }
+
+        return position;
+    }
 
     /**
      * returns the current angle of the motor
